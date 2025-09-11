bb16230c18d75ddfdecbe6cbd1879223cf5fcc74
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-10 21:21:08 -0700

    Added logic for making a new save
    Right now it just has a saves folder and every time you click done in
    the new saves menu it makes a folder in the saves folder with a
    save.json but that file is empty right now. And also there will probably
    be more json files eventually.

--- Scripts/Globals.cs
+++ Scripts/Globals.cs
@@ -5,4 +5,18 @@ public partial class Globals : Node {
     public static string LatestVersion = "";
     public static string AccessLevel = "";
     public static bool VersionChecked = false;
+
+    public static Godot.Collections.Dictionary<string, Variant> SaveJson = [];
+
+    public static void CreateSavesDir() {
+        string folderPath = "user://saves";
+
+        var dir = DirAccess.Open(folderPath);
+        if (dir != null) {
+            GD.Print("Folder exists: " + folderPath);
+        } else {
+            GD.Print("Creating saves folder");
+            DirAccess.MakeDirAbsolute(ProjectSettings.GlobalizePath(folderPath));
+        }
+    }
 }
---
+++ Scripts/NewSaveCreation.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class NewSaveCreation : Button {
+    public void _on_button_up() {
+        Globals.CreateSavesDir();
+
+        string SaveName = GetNode<LineEdit>("../SaveNameTextBox").Text;
+        string SaveFolderName = SaveName
+            .Replace("/", "-")
+            .Replace("\\", "-")
+            .Replace(":", "-")
+            .Replace("*", "-")
+            .Replace("?", "-")
+            .Replace("\"", "-")
+            .Replace("<", "-")
+            .Replace(">", "-")
+            .Replace("|", "-");
+
+        string SaveFolder = "user://saves/" + SaveFolderName;
+        GD.Print("Creating Save: " + SaveFolder);
+        DirAccess.MakeDirAbsolute(ProjectSettings.GlobalizePath(SaveFolder));
+
+        using var file = FileAccess.Open(SaveFolder + "/" + "save.json", FileAccess.ModeFlags.Write);
+
+        Globals.SaveJson = new Godot.Collections.Dictionary<string, Variant>();
+
+        Globals.SaveJson["savename"] = SaveName;
+        Globals.SaveJson["savefolder"] = SaveFolderName;
+        Globals.SaveJson["playerposition"] = "1;0:0:1000";
+        Globals.SaveJson["inventory"] = new Godot.Collections.Array<string>();
+
+        GD.Print(Json.Stringify(Globals.SaveJson));
+        // file.StoreString(Json.Stringify(Globals.SaveJson));
+    }
+}
