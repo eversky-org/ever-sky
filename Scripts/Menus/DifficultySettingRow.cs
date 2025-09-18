df02b5c5efde73f989fb2f7494713d063556b9ec
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-17 20:58:29 -0700

    Made the value of pvp and base-building actually save
    It doesnt display any difference when u change the setting yet but it
    will later. Also I havent done the other 2 settings yet cuz they work
    differently since they are textboxes

--- Scripts/Menus/DifficultySettingRow.cs
+++ Scripts/Menus/DifficultySettingRow.cs
@@ -3,14 +3,16 @@ using System;
 
 public partial class DifficultySettingRow : ColorRect {
     [Export(PropertyHint.MultilineText)] public string HoverText = "";
-    [Export] public bool IsTextBoxSetting = true;
+    [Export] public string SettingName = "";
+    [Export(PropertyHint.Enum, "Float,NoOne Group Anyone")]
+    public string SettingType { get; set; } = "Float";
 
     public override void _Ready() {
         MouseEntered += Hovered;
     }
 
     public override void _Process(double delta) {
-        if (IsTextBoxSetting) {
+        if (SettingType == "Float") {
             LineEdit Input = GetNode<LineEdit>("Input");
 
             foreach (char character in Input.Text) {
@@ -21,6 +23,18 @@ public partial class DifficultySettingRow : ColorRect {
 
             if (Input.Text.EndsWith('.') || Input.Text.StartsWith('.')) {
                 Input.Text = "1.0";
+            }
+        } else if (SettingType == "NoOne Group Anyone") {
+            Button NoOne = GetNode<Button>("Buttons/NoOne");
+            Button Group = GetNode<Button>("Buttons/Group");
+            Button Anyone = GetNode<Button>("Buttons/Anyone");
+
+            if (NoOne.ButtonPressed) {
+                Globals.DifficultySettings[SettingName] = "no-one";
+            } else if (Group.ButtonPressed) {
+                Globals.DifficultySettings[SettingName] = "group";
+            } else if (Anyone.ButtonPressed) {
+                Globals.DifficultySettings[SettingName] = "anyone";
             }
         }
     }
